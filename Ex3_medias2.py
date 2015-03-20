38bfbfc46455ed8aae37f2d1109b4b4f905e8977
MageLagua <magelagua@gmail.com>
2015-03-20 11:32:49 -0300

    Create Ex3_medias2.py

---
+++ Ex3_medias2.py
@@ -0,0 +1,12 @@
+nota=0
+tot=0
+num=0
+while(nota>=0):
+  nota=int(raw_input("digite a nota: "))
+  if(nota>=0):
+    notalist.append(nota)
+    num+=1
+for i in range(num):
+  tot+=notalist[i]
+tot=tot/num
+ptint("a media do aluno e: {0}".format(tot))
