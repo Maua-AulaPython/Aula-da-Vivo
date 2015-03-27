91d05e79ed121953560b46cd35032e97a2064446
MageLagua <magelagua@gmail.com>
2015-03-27 12:09:55 -0300

    Create Ex4_Trabalho1.py

---
+++ Ex4_Trabalho1.py
@@ -0,0 +1,13 @@
+def Verifica(A, B, C):
+    if (C**2 == A**2+B**2):
+        print 'A area vale {0}!'.format(A*B/2)
+    else:
+        print 'Os valores A = {0}, B = {1}, C = {2} nao formam um triangulo retangulo!'.format(A, B, C)
+
+A = int(raw_input('Digite o primeiro cateto: '))
+B = int(raw_input('Digite o segundo cateto: '))
+C = int(raw_input('Digite a hipotenusa: '))
+if (C>A+B):
+    print('Nao eh um triangulo!')
+else:
+    Verifica(A, B, C)
